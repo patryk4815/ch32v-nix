ee40e897201afc9f65188ed542a824cf127acafb
Patryk Sondej <patryk.sondej@gmail.com>
2025-08-12 02:27:53 +0200

    nix fmt

--- blackmagic.nix
+++ blackmagic.nix
@@ -1,18 +1,21 @@
 {
-   pkgs ? (import (builtins.getFlake "github:nixos/nixpkgs/nixpkgs-unstable") {})
+  pkgs ? (import (builtins.getFlake "github:nixos/nixpkgs/nixpkgs-unstable") { }),
 }:
 let
-    # https://github.com/blackmagic-debug/blackmagic/pull/1399
-    lib = pkgs.lib;
-    final = pkgs.blackmagic.overrideAttrs (old: {
-      version = "1.10.0";
-      src = pkgs.fetchFromGitHub {
-          owner = "perigoso";
-          repo = "blackmagic";
-          rev = "a0daa1d0c635a8ab49f205aee7bddc31fc2bcf6f";  # feature/wch-link-support
-          hash = "sha256-0sdwJ3CA8iPy2w2gjVys+mTkb/bhgG1FTt7VEaDDzyw=";
-      };
-      mesonFlags = [ "-Drvswd_support=true" ];
-    });
+  # https://github.com/blackmagic-debug/blackmagic/pull/2061
+  lib = pkgs.lib;
+  final = pkgs.blackmagic.overrideAttrs (old: {
+    version = "1.10.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "perigoso";
+      repo = "blackmagic";
+      rev = "a0daa1d0c635a8ab49f205aee7bddc31fc2bcf6f"; # feature/wch-link-support
+      hash = "sha256-0sdwJ3CA8iPy2w2gjVys+mTkb/bhgG1FTt7VEaDDzyw=";
+    };
+    mesonFlags = [
+      "-Drvswd_support=true"
+      "-Dtargets=riscv32,ch32,ch32v"
+    ];
+  });
 in
-    final
+final
